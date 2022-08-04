75fe4ab5b92d6809dc29eb46ac6b03d65d8ae644
gjames-gss <gjames@gssmail.com>
2022-08-04 11:17:23 -0500

    Added Projects

---
+++ TestedTrek/CustomUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36c5f998-b7ea-4779-bd74-1484dd15e696")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestedTrek/CustomUnitTests/ShieldTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CustomUnitTests
+{
+    [TestClass]
+    public class ShieldTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
